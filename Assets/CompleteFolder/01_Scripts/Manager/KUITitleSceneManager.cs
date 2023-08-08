5c843c673fd6c7c9c82dae1c9eae4f0acba46d50
Kevin04261004 <104260597+Kevin04261004@users.noreply.github.com>
2023-08-08 23:04:20 +0900

    dd
    dd

--- Assets/CompleteFolder/01_Scripts/Manager/KUITitleSceneManager.cs
+++ Assets/CompleteFolder/01_Scripts/Manager/KUITitleSceneManager.cs
@@ -6,17 +6,17 @@ using UnityEngine.UI;
 
 public class KUITitleSceneManager : MonoBehaviour
 {
-    [SerializeField] private Image _startGameBG_Image;
+    [SerializeField] private Image _name_Input_Image;
     public void OnClick_GameStart_Btn()
     {
-        _startGameBG_Image.gameObject.SetActive(true);
+        _name_Input_Image.gameObject.SetActive(true);
     }
     public void OnClick_GameStartExit_Btn()
     {
-        _startGameBG_Image.gameObject.SetActive(false);
+        _name_Input_Image.gameObject.SetActive(false);
     }
     public void OnClick_ChangeTo_GameScene()
     {
-        SceneManager.LoadScene("KGameScene");
+        SceneManager.LoadScene("01_GameScene");
     }
 }
