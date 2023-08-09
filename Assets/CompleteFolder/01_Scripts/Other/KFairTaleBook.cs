b9c61742b46f8d86abe189228d6367071fdfabde
Kevin04261004 <104260597+Kevin04261004@users.noreply.github.com>
2023-08-10 01:14:06 +0900

    중간저장
    ㅇㅇ

--- Assets/CompleteFolder/01_Scripts/Other/KFairTaleBook.cs
+++ Assets/CompleteFolder/01_Scripts/Other/KFairTaleBook.cs
@@ -1,13 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public enum EFairTaleBook_State
 {
-    First,
-    Second,
-    Third,
-    
+    Act_Second,
+    Act_Third,
 }
 
 public class KFairTaleBook : MonoBehaviour
@@ -15,9 +14,23 @@ public class KFairTaleBook : MonoBehaviour
     [SerializeField] private EFairTaleBook_State _state;
     [SerializeField] private GameObject B_ForAct2;
     [SerializeField] private GameObject B_ForAct2_After_GetBook;
+    [SerializeField] private GameObject C_ForAct3;
+    [SerializeField] private GameObject C_ForAct3_After_GetBook;
+    
     public void Used()
     {
-        B_ForAct2.SetActive(false);
-        B_ForAct2_After_GetBook.SetActive(true);
+        switch (_state)
+        {
+            case EFairTaleBook_State.Act_Second:
+                B_ForAct2.SetActive(false);
+                B_ForAct2_After_GetBook.SetActive(true);
+                break;
+            case EFairTaleBook_State.Act_Third:
+                C_ForAct3.SetActive(false);
+                C_ForAct3_After_GetBook.SetActive(true);
+                break;
+            default:
+                break;
+        }
     }
 }
