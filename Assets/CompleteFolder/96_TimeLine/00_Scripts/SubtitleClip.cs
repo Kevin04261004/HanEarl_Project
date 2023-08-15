344633d52fc615bb4c1389d88b73f85231a77fa8
Kevin04261004 <104260597+Kevin04261004@users.noreply.github.com>
2023-08-15 12:57:05 +0900

    타임라인이 런타임에서만 이름 변경되게 바꿈
    ㅇㅇ

--- Assets/CompleteFolder/96_TimeLine/00_Scripts/SubtitleClip.cs
+++ Assets/CompleteFolder/96_TimeLine/00_Scripts/SubtitleClip.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,12 +9,22 @@ public class SubtitleClip : PlayableAsset
     [TextArea(3,5)]
     public string _subtitle_TMP;
 
+    private bool _isRuntime = false;
+
+    private void Awake()
+    {
+        _isRuntime = true;
+    }
+
     public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
     {
         var playable = ScriptPlayable<SubtitleBehaviour>.Create(graph);
 
         SubtitleBehaviour subtitleBehaviour = playable.GetBehaviour();
-        _subtitle_TMP = _subtitle_TMP.Replace("플레이어", JDataManager.instance.playerData.name);
+        if (_isRuntime)
+        {
+            _subtitle_TMP = _subtitle_TMP.Replace("플레이어", JDataManager.instance.playerData.name);
+        }
         subtitleBehaviour._subtitle_TMP = _subtitle_TMP;
 
         return playable;
