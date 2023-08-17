a1a6741b926ced67bc1ea59bc60f0fcb72c2019b
GwonCheonmyeong <130067941+GwonCheonmyeong@users.noreply.github.com>
2023-08-17 21:13:27 +0900

    Act5 엔딩 수정 및 엔딩 게시판
    엔딩 게시판추가 및 배드엔딩 수정

--- Assets/CompleteFolder/01_Scripts/Stage/G_EndingManager.cs
+++ Assets/CompleteFolder/01_Scripts/Stage/G_EndingManager.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -18,12 +19,42 @@ public class G_EndingManager : MonoBehaviour
 {
     public EndingType currentEndingName;
     public Dictionary<string, bool> endingList = new Dictionary<string, bool>();
-
     [field:SerializeField] public bool normalEndingCheck { get; private set; } = false;
+    [field: SerializeField] public bool trueEndingCheck { get; private set; } = false;
+
+    [SerializeField] private GameObject[] endingTextObj;
+    [SerializeField] private GameObject normalEndingText;
+    [SerializeField] private GameObject trueEndingText;
 
     public void CallEnding(string endingName) // stage매니저 에게서 정보를 받고 엔딩을 호출함
     {
         EndingTypeSetting(endingName);
+    }
+
+    public void Endinginitialization(string endingName)
+    {
+        EndingClearSetting(endingName);
+    }
+
+    private void EndingClearSetting(string endingName)
+    {
+        for (int i = 0; i < endingTextObj.Length; i++)
+        {
+            if (endingTextObj[i].name == endingName)
+            {
+                endingTextObj[i].SetActive(true);
+            }
+        }
+
+        if (G_DifurcationManager.Instance.NormalChangeEndingCheck())
+        {
+            normalEndingText.SetActive(true);
+        }
+
+        if (G_DifurcationManager.Instance.TrueChangeEndingCheck())
+        {
+            trueEndingText.SetActive(true);
+        }
     }
 
     private void EndingTypeSetting(string endingName)
@@ -57,6 +88,8 @@ public class G_EndingManager : MonoBehaviour
                 break;
             case EndingType.TrueEnding:
                 endingList.Add(endingName, true);
+                trueEndingCheck = true;
+                G_DifurcationManager.Instance.TrueEndingStart();
                 break;
             default:
                 break;
